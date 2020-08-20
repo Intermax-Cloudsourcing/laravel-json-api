559d01a8b5b42f36ab329310ec8ae877a4595431
Patrick Hoogkamer <p.hoogkamer@intermax.nl>
2020-08-20 13:43:12 +0200

    feat: Add basic json api response

--- src/JsonApi/Resources/JsonApiResource.php
+++ src/JsonApi/Resources/JsonApiResource.php
@@ -2,7 +2,60 @@
 
 namespace Intermax\LaravelApi\JsonApi\Resources;
 
-class JsonApiResource
-{
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionException;
 
+abstract class JsonApiResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ReflectionException
+     */
+    public function toResponse($request)
+    {
+        $data = $this->resolve($request);
+
+        $data = $this->transformToJsonApi($data);
+
+        $json = array_merge_recursive(
+            [
+                static::$wrap => $data
+            ],
+            $this->with($request),
+            $this->additional
+        );
+
+        return response()->json($json);
+    }
+
+    /**
+     * @param $data
+     * @return array
+     * @throws ReflectionException
+     */
+    protected function transformToJsonApi($data): array
+    {
+        $class = new ReflectionClass($this->resource);
+
+        $jsonApiData = [
+            'id' => $data['id'],
+            'type' => Str::plural(strtolower($class->getShortName())),
+            'attributes' => [],
+        ];
+
+        foreach ($data as $key => $attribute) {
+            if ($key == 'id') {
+                continue;
+            }
+
+            $jsonApiData['attributes'][$key] = $attribute;
+        }
+
+        return $jsonApiData;
+    }
 }
