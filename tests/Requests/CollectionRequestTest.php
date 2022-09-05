c8bb88c06a3bf5d6d1ea3f9175065381544d375e
Patrick Hoogkamer <patrickhoogkamer@gmail.com>
2022-09-05 16:49:49 +0200

    feat: adapt open api 1 changes (#10)
    * chore: update laravel open api
    
    * feat: implement HasQueryParameters in base request
    
    Co-authored-by: Patrick Hoogkamer <p.hoogkamer@intermax.nl>

---
+++ tests/Requests/CollectionRequestTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Intermax\LaravelApi\Tests\Resources\JsonApi;
+
+use Illuminate\Database\Eloquent\Collection;
+use Intermax\LaravelApi\JsonApi\Filters\OperatorFilter;
+use Intermax\LaravelApi\JsonApi\Filters\ScopeFilter;
+use Intermax\LaravelApi\JsonApi\Requests\FilterRequest;
+use Orchestra\Testbench\TestCase;
+
+class CollectionRequestTest extends TestCase
+{
+    /** @test */
+    public function it_outputs_parameters(): void
+    {
+        $request = new class() extends FilterRequest
+        {
+            public function filters(): array
+            {
+                return [
+                    new OperatorFilter(
+                        fieldName: 'test',
+                        allowedOperators: ['eq', 'gt', 'lte'],
+                    ),
+                    new ScopeFilter(
+                        fieldName: 'scope',
+                    ),
+                ];
+            }
+        };
+
+        $queryParameters = new Collection($request->queryParameters());
+
+        $this->assertNotNull($queryParameters->where('name', 'filter[test]'));
+        $this->assertNotNull($queryParameters->where('name', 'filter[test][eq]'));
+        $this->assertNotNull($queryParameters->where('name', 'filter[test][gt]'));
+        $this->assertNotNull($queryParameters->where('name', 'filter[test][lte]'));
+        $this->assertNotNull($queryParameters->where('name', 'filter[scope]'));
+    }
+}
